e44f9a3a72eca40de4e2cc9fb55cd8225240ad40
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-28 19:02:52 -0300

    Utility types

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,12 +6,7 @@ export type Guitar = {
     price: number
 }
 
-export type CartItem = {
-    id : number
-    name: string
-    image: string
-    description: string
-    price: number
+export type CartItem = Guitar &{
     quantity: number
 }
 
