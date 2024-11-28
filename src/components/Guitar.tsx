3e814f8fe551f8ffc0b6c7d036d08054fc0ce9e2
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-28 17:56:35 -0300

    Archivo types

--- src/components/Guitar.tsx
+++ src/components/Guitar.tsx
@@ -1,11 +1,4 @@
-type Guitar = {
-    id : number
-    name: string
-    image: string
-    description: string
-    price: number
-}
-
+import type {Guitar} from "../types"
 type GuitarProps = {
     guitar : Guitar,
     addToCart: (item: Guitar) => void
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type Guitar = {
+    id : number
+    name: string
+    image: string
+    description: string
+    price: number
+}
+
