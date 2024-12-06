f485b187a67e1f312e13fcaee6db74bc86e20186
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-06 16:55:48 -0300

    Definiendo el state initial y su type

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -1,5 +1,7 @@
-import { Guitar } from "../types" 
+import {db} from '../data/db.js'
+import {Guitar, CartItem} from "../types" 
 
+// Actions
 export type CartActions = 
     {type: 'add-to-cart', payload: {item: Guitar}} |
     {type: 'remove-from-cart', paylod: {id: Guitar['id']}} |
@@ -7,3 +9,14 @@ export type CartActions =
     {type: 'increase-quantity', paylod: {id: Guitar['id']}} |
     {type: 'clear-cart'} 
 
+// Type State
+export type CartState = {
+    data: Guitar[]
+    cart: CartItem[]
+}
+
+// Initial State
+export const initialState : CartState = {
+    data: db,
+    cart: []
+}
